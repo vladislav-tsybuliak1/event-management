6b26c1320089dcaf5fe0805edfc6a412727628d5
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-12 00:28:37 +0200

    Add necessary imports

--- events/models.py
+++ events/models.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.timezone import now
 
 
 class Event(models.Model):
