92b77376c9535327dae3702be9fab15d39ffe3cd
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-11 17:10:34 +0200

    Add routing for user creation

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from users.views import CreateUserView
+
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+]
+
+app_name = "users"
