994b847ddf13a29d0183a1314f31b841eaaa8618
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-12 18:39:51 +0200

    Add sending successful registration email notification

--- events/views.py
+++ events/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.db.models import QuerySet
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
@@ -8,6 +10,12 @@ from rest_framework.permissions import (
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from email_templates.event_registration_template import (
+    REGISTRATION_HTML_CONTENT,
+)
+from email_templates.event_cancel_registration_template import (
+    CANCEL_REGISTRATION_HTML_CONTENT,
+)
 from events.models import Event
 from events.permissions import IsOrganizerOrReadOnly
 from events.serializers import (
@@ -68,6 +76,25 @@ class EventViewSet(viewsets.ModelViewSet):
                 {"detail": "You are already registered for this event."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+
+        # Sending email about successful registration
+        subject = f"You are registered at {event.title}"
+        message = REGISTRATION_HTML_CONTENT.format(
+            username=request.user.username,
+            event=event.title,
+            start_time=event.start_time.strftime("%d %b %Y %H:%M"),
+            end_time=event.end_time.strftime("%d %b %Y %H:%M"),
+            location=event.location,
+            organizer_email=event.organizer.email,
+        )
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[request.user.email],
+            fail_silently=True,
+            html_message=message,
+        )
 
         # Register the user
         event.participants.add(request.user)
