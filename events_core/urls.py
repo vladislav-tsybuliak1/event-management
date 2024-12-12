ac2eebeed1809ed7965929334eca43274e46d716
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-13 00:22:02 +0200

    Add spectacular drf urls

--- events_core/urls.py
+++ events_core/urls.py
@@ -14,15 +14,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/users/", include("users.urls", namespace="users")),
     path("api/v1/events/", include("events.urls", namespace="events")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/v1/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
+    path(
+        "api/v1/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 urlpatterns += debug_toolbar_urls()
