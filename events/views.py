a1225b393ea50fdd92bb04a75a8c9a17b2a50863
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-12 12:04:37 +0200

    Edit event view by adding create/update serializer

--- events/views.py
+++ events/views.py
@@ -2,7 +2,8 @@ from django.db.models import QuerySet
 from rest_framework import viewsets
 
 from events.models import Event
-from events.serializers import EventSerializer, EventListSerializer
+from events.serializers import EventSerializer, EventListSerializer, \
+    EventCreateUpdateSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -12,9 +13,14 @@ class EventViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self) -> type[EventSerializer]:
         if self.action == "list":
             return EventListSerializer
+        if self.action in ["create", "update", "partial_update"]:
+            return EventCreateUpdateSerializer
         return self.serializer_class
 
     def get_queryset(self) -> QuerySet:
         if self.action == "list":
             self.queryset = self.queryset.select_related("organizer")
         return self.queryset
+
+    def perform_create(self, serializer: EventCreateUpdateSerializer):
+        serializer.save(organizer=self.request.user)
