228fed991a51312c47076d772e6ce729c5f6b9d9
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-12 02:10:48 +0200

    Add urld and settings for django debug toolbar

--- events_core/urls.py
+++ events_core/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
@@ -24,3 +24,5 @@ urlpatterns = [
     path("api/v1/users/", include("users.urls", namespace="users")),
     path("api/v1/events/", include("events.urls", namespace="events")),
 ]
+
+urlpatterns += + debug_toolbar_urls()
